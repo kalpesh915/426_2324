092e56c15f9beca6150ad676dfdb2ced513cfdf0
Kalpesh Chauhan <63454815+kalpesh915@users.noreply.github.com>
2023-08-12 17:04:13 +0530

    12-8

---
+++ C Language/pro72.c
@@ -0,0 +1,8 @@
+/**
+    escape sequence is used to print any
+    special character in string
+*/
+
+void main(){
+    printf("\n welcome to \n world of \n \"c language\"");
+}
---
+++ C Language/pro73.c
@@ -0,0 +1,16 @@
+/**
+    constant
+*/
+
+void main(){
+    int x = 10;
+    printf("\n value of x is %d", x);
+
+    x = 015; /// octal number
+    printf("\n value of x is %d", x);
+    printf("\n value of x is %o", x);
+
+    x = 0x34; /// hex decimal number
+    printf("\n value of x is %d", x);
+    printf("\n value of x is %x", x);
+}
